fb08c6babecd76ac37f937ee71c2f354003fa999
Damilare Joseph <dre4success@live.com>
2017-09-14 16:41:38 +0100

    resources component

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+	return (
+		<div>
+			Super Special Secret Recipe
+			<ul>
+				<l1>1 Cup Sugar</l1>
+				<l1>1 Cup Pepper</l1>
+				<l1>1 Cup Salt</l1>
+			</ul>
+		</div>
+	)
+}
